9b6a5fffd524e6d4a0792e61c3a5819f88fac110
Michael Kishinevsky <mkishine@yahoo.com>
2019-03-15 07:44:11 -0400

    Interacting components

--- src/App.js
+++ src/App.js
@@ -16,18 +16,29 @@ const list = [
     },
 ];
 
+const isSearched = (query) => (item) => !query|| item.title.indexOf(query) !== -1;
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list
+            list,
+            query: ''
         };
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
 
+    onSearchChange(event) {
+        this.setState({query: event.target.value});
+    }
     render() {
+        const {query, list} = this.state;
         return (
             <div className="App">
-                {this.state.list.map(item =>
+                <form>
+                    <input type="text" value={query} onChange={this.onSearchChange}/>
+                </form>
+                {list.filter(isSearched(query)).map(item =>
                     <div key={item.objectId}>
                         <span><a href={item.url}>{item.title}</a></span>
                         <span>{item.author}</span>
